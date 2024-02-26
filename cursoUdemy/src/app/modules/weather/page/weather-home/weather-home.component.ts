53a4bc8505906b32ef787105fefc56e499d8a4ea
Lucas-Lion <lucaslion350@gmail.com>
2024-02-26 16:19:46 -0300

    estudos api

--- cursoUdemy/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ cursoUdemy/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: ['./weather-home.component.css']
+  styleUrls: []
 })
 export class WeatherHomeComponent {
+  constructor(private weatherService: WeatherService) {}
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+      }
+    })
+  }
 
 }
